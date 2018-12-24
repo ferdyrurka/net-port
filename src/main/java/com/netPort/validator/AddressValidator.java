0e046e1876fef44ef62be0223f167eb1b6e55979
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-12-24 23:05:36 +0100

    Bug IP validate

--- src/main/java/com/netPort/validator/AddressValidator.java
+++ src/main/java/com/netPort/validator/AddressValidator.java
@@ -7,7 +7,7 @@ public class AddressValidator implements ValidatorInterface<String> {
 
     public boolean validate(String domain){
         if (!domain.matches("^([A-Z|a-z|-|_|0-9|.]{1,70}).([a-z|A-Z]{1,10})$")
-            && !domain.matches("^([0-9]{3}).([0-9]{3}).([0-9]{1,3}).([0-9]{1,3})$")) {
+            && !domain.matches("^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$")) {
             return false;
         }
 
